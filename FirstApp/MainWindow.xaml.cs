6f99e2d572cc4159719d361fefeb7e6200cc026c
foka <michal.foka.wlodarczyk@gmail.com>
2013-08-27 15:51:35 +0200

    Dummy depth

--- FirstApp/MainWindow.xaml.cs
+++ FirstApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit;
 using Microsoft.Samples.Kinect.WpfViewers;
@@ -50,7 +53,67 @@ namespace FirstApp
 		}
 
 		void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+		{ 
+			using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+			{
+				if (depthFrame == null)
+					return;
+
+				byte[] pixels = GenerateColoredBytes(depthFrame);
+
+				int stride = depthFrame.Width * 4;
+				depthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
+					96, 96, PixelFormats.Bgr32, null, pixels, stride);
+			}
+		}
+
+		private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
 		{
+			short[] rawDepthData = new short[depthFrame.PixelDataLength];
+			depthFrame.CopyPixelDataTo(rawDepthData);
+
+			byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+			const int BlueIndex = 0;
+			const int GreenIndex = 1;
+			const int RedIndex = 2;
+
+			for (int depthIndex = 0, colorIndex = 0;
+				depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+				depthIndex++, colorIndex+=4)
+			{
+				int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+				int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+				if (depth <= 900)
+				{
+					pixels[colorIndex + BlueIndex] = 255;
+					pixels[colorIndex + GreenIndex] = 0;
+					pixels[colorIndex + RedIndex] = 0;
+				}
+				else if (depth > 900 && depth < 2000)
+				{
+					pixels[colorIndex + BlueIndex] = 0;
+					pixels[colorIndex + GreenIndex] = 255;
+					pixels[colorIndex + RedIndex] = 0;
+				}
+				else if (depth > 2000)
+				{
+					pixels[colorIndex + BlueIndex] = 0;
+					pixels[colorIndex + GreenIndex] = 0;
+					pixels[colorIndex + RedIndex] = 255;
+				}
+
+
+				if (player > 0)
+				{
+					pixels[colorIndex + BlueIndex] = Colors.Pink.B;
+					pixels[colorIndex + GreenIndex] = Colors.Pink.G;
+					pixels[colorIndex + RedIndex] = Colors.Pink.R;
+				}
+			}
+
+			return pixels;
 		}
 
 		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
